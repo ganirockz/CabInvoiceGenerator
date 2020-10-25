a610befd7d99e827076757fc04f9e353728d7103
ganirockz <ganeshrapeti8@gmail.com>
2020-10-25 20:44:33 +0530

    [Ganesh]Added: given multiple rides,calculate total fare for journey

--- src/main/java/com/capgemini/InvoiceGenerator.java
+++ src/main/java/com/capgemini/InvoiceGenerator.java
@@ -1,5 +1,7 @@
 package com.capgemini;
 
+import java.util.ArrayList;
+
 public class InvoiceGenerator {
 	public final int COST_PER_KILOMETER = 10;
 	public final int COST_PER_TIME = 1;
@@ -8,6 +10,14 @@ public class InvoiceGenerator {
 	public double calculateFare(double distance, int time) {
 		double fare = distance * COST_PER_KILOMETER + time * COST_PER_TIME;
 		return Math.max(MINIMUM_FARE, fare);
+	}
+
+	public double calculateFare(ArrayList<Ride> rides) {
+		double fare = 0;
+		for(Ride ride:rides) {
+			fare +=calculateFare(ride.getDistance(),ride.getTime());
+		}
+		return fare;
 	}
 
 }
